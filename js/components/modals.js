97bf3b801ecb627adead0c423754cc0aac607219
moumi manoah <manomoumi@gmail.com>
2022-11-15 13:03:09 +0100

    arranging index page moving services section

--- js/components/modals.js
+++ js/components/modals.js
@@ -13,6 +13,8 @@ const inputLocation = document.querySelector('#location-input'),
   modalLocation = document.querySelector('#modal-location'),
   modalAlert = document.querySelector('#modal-danger')
 const modalOverlay = document.querySelector('.modal-overlay')
+const infoLocationCart = document.querySelector('.info')
+// console.log(infoLocationCart)
 
 // console.log(serviceList, locationList)
 
@@ -23,16 +25,91 @@ function limit(string = '', limit = 0) {
 if (modalOverlay) {
   const locationList = modalLocation.querySelector('.list')
   const serviceList = modalServices.querySelector('.list')
-  /*** on click */
-  inputServices.addEventListener('click', () => {
-    modalServices.classList.add('show')
-  })
+  const services = serviceList.querySelectorAll('input')
+  const btn = serviceList.parentElement.querySelector('.btn')
+  /******************** for search form ***********************/
+  if (inputLocation) {
+    inputServices.addEventListener('click', () => {
+      modalServices.classList.add('show')
+    })
+    inputLocation.addEventListener('click', () => {
+      modalLocation.classList.add('show')
+    })
+
+    /*** for input date */
+    inputDate.addEventListener('input', () => {
+      if (inputDate.value) {
+        const clearBtn = inputDate.parentElement.querySelector('i:nth-child(2)')
+        console.log(closeBtn)
+        clearBtn.style.display = 'block'
+        clearBtn.addEventListener('click', () => {
+          // inputDate.value = ''
+          calendar.clear()
+          clearBtn.style.display = 'none'
+        })
+      }
+    })
+
+    /* getting values to add it on search form */
+    // for location
+    locationList.addEventListener('click', (e) => {
+      // console.log(e.target)
+      if (e.target.hasAttribute('data-location')) {
+        let location = e.target.dataset.location
+        // console.log(location)
+
+        if (location.length > 25) {
+          location = limit(location, 25).concat('...')
+        }
+        inputLocation.value = location
+        if (inputLocation.value != '') {
+          modalLocation.classList.remove('show')
+          const clearBtn = inputLocation.parentElement.querySelector('i:nth-child(2)')
+          clearBtn.style.display = 'block'
+          clearBtn.addEventListener('click', () => {
+            inputLocation.value = ''
+            clearBtn.style.display = 'none'
+          })
+        }
+      }
+    })
+    // for services
+    btn.addEventListener('click', () => {
+      let checkedList = [...new Set()]
+      let values = []
+
+      services.forEach((service) => {
+        if (service.checked) {
+          checkedList.push(service.value)
+          values = checkedList.join(', ')
+          console.log(values)
+        }
+      })
+      modalServices.classList.remove('show')
+      if (values != '') {
+        if (values.length > 25) {
+          values = limit(values, 25).concat('...')
+          console.log(values)
+        }
+        inputServices.value = values
+        const clearBtn = inputServices.parentElement.querySelector('i:nth-child(2)')
+        clearBtn.style.display = 'block'
+        clearBtn.addEventListener('click', () => {
+          inputServices.value = ''
+          clearBtn.style.display = 'none'
+          services.forEach((service) => {
+            if (service.checked) {
+              service.checked = false
+            }
+          })
+        })
+      } else {
+        inputServices.value = ''
+      }
+    })
+  }
   modalServices.querySelector('.close').addEventListener('click', () => {
     modalServices.classList.remove('show')
-  })
-
-  inputLocation.addEventListener('click', () => {
-    modalLocation.classList.add('show')
   })
 
   modalLocation.querySelector('.close').addEventListener('click', () => {
@@ -42,69 +119,21 @@ if (modalOverlay) {
     modalAlert.classList.remove('show')
   })
 
-  /* getting values to add it on search form */
+  /*********** for card form ********/
+
+  const destinationBtn = document.querySelector('#destination-btn')
+  destinationBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    modalLocation.classList.add('show')
+  })
   locationList.addEventListener('click', (e) => {
-    // console.log(e.target)
     if (e.target.hasAttribute('data-location')) {
       let location = e.target.dataset.location
-      console.log(location)
-      if (location.length > 25) {
-        location = limit(location, 25).concat('...')
-      }
-      inputLocation.value = location
-      if (inputLocation.value != '') {
+      if (infoLocationCart) {
+        infoLocationCart.innerHTML = `<p>${location}</p>`
+        // console.log(infoLocationCart)
         modalLocation.classList.remove('show')
-        const closeBtn = inputLocation.parentElement.querySelector('i:nth-child(2)')
-        closeBtn.style.display = 'block'
-        closeBtn.addEventListener('click', () => {
-          inputLocation.value = ''
-          closeBtn.style.display = 'none'
-        })
       }
-    }
-  })
-  const services = serviceList.querySelectorAll('input')
-  const btn = serviceList.parentElement.querySelector('.btn')
-  btn.addEventListener('click', () => {
-    let checkedList = [...new Set()]
-    let values = []
-
-    services.forEach((service) => {
-      if (service.checked) {
-        checkedList.push(service.value)
-        values = checkedList.join(', ')
-        console.log(values)
-      }
-    })
-    modalServices.classList.remove('show')
-    if (values != '') {
-      if (values.length > 25) {
-        values = limit(values, 25).concat('...')
-        console.log(values)
-      }
-      inputServices.value = values
-      const closeBtn = inputServices.parentElement.querySelector('i:nth-child(2)')
-      closeBtn.style.display = 'block'
-      closeBtn.addEventListener('click', () => {
-        inputServices.value = ''
-        closeBtn.style.display = 'none'
-      })
-    } else {
-      inputServices.value = ''
-    }
-  })
-
-  /*** for input date */
-  inputDate.addEventListener('input', () => {
-    if (inputDate.value) {
-      const closeBtn = inputDate.parentElement.querySelector('i:nth-child(2)')
-      console.log(closeBtn)
-      closeBtn.style.display = 'block'
-      closeBtn.addEventListener('click', () => {
-        // inputDate.value = ''
-        calendar.clear()
-        closeBtn.style.display = 'none'
-      })
     }
   })
 }
